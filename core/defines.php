71c497c87533ba65ba242b6a361e13d0cd747058
Engrev <tvergne83@gmail.com>
2020-05-01 18:29:40 +0200

    Updates
    Updating files README.md and composer.json.

--- core/defines.php
+++ core/defines.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Europe/Berlin');
 setlocale(LC_TIME, 'fr_FR.utf8', 'fra');
-//var_dump($_SERVER['SCRIPT_FILENAME'], $_SERVER['REQUEST_URI'], $_SERVER['PHP_SELF']);
+
 define('_DEBUG_', true);
 define('_PATH_', '/edsa-blog');
