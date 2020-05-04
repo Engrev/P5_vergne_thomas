fed0abfb81b1de8c976dc7219dcc2321a7576559
Engrev <tvergne83@gmail.com>
2020-05-04 15:54:57 +0200

    Error pages
    #9 Creation of error pages.

--- core/RouterException.php
+++ core/RouterException.php
@@ -9,5 +9,22 @@ namespace App\Core;
  */
 class RouterException extends \Exception
 {
-    
+    const http_codes = [
+        '400' => 'Bad Request',
+        '401' => 'Unauthorized',
+        '403' => 'Forbidden',
+        '404' => 'Not Found',
+        '405' => 'Method Not Allowed',
+        '500' => 'Internal Server Error',
+        '502' => 'Bad Gateway ou Proxy Error',
+        '503' => 'Service Unavailable'
+    ];
+
+    public function display($http_code)
+    {
+        if (is_int($http_code) && in_array($http_code, self::http_codes)) {
+            header('HTTP/1.0 '.$http_code.' '.self::http_codes[$http_code]);
+            echo Session::getInstance()->read('Twig')->render('errors/'.$http_code.'.twig', ['error_message'=>$this->getMessage()]);
+        }
+    }
 }
--- index.php
+++ index.php
@@ -13,41 +13,45 @@ $Twig->addGlobal('_js_', []);
 if (_DEBUG_ === true) {
     $Twig->addExtension(new Twig\Extension\DebugExtension());
 }
-$Session = \App\Core\Session::getInstance();
+$Session = App\Core\Session::getInstance();
 $Session->write('Twig', $Twig);
 $Router = new App\Core\Router($_GET['url']);
 
-// FRONT
-$Router->get('/', 'Home#display');
+try {
+    // FRONT
+    $Router->get('/', 'Home#display');
 
-$Router->get('/derniers-articles', 'Posts#last');
-$Router->get('/articles/:id-:slug', 'Posts#list')->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+');
+    $Router->get('/derniers-articles', 'Posts#last');
+    $Router->get('/articles/:id-:slug', 'Posts#list')->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+');
 
-$Router->get('/categories', 'Categories#listAll');
-$Router->get('/categories/:id-:slug', 'Categories#list')->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+');
+    $Router->get('/categories', 'Categories#listAll');
+    $Router->get('/categories/:id-:slug', 'Categories#list')->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+');
 
-/*$Router->get('/contact', function () use ($Twig) { echo $Twig->render('front/contact.twig'); });
-$Router->get('/connexion', function () use ($Twig) { echo $Twig->render('front/login.twig'); });
-$Router->get('/inscription', function () use ($Twig) { echo $Twig->render('front/register.twig'); });*/
+    /*$Router->get('/contact', function () use ($Twig) { echo $Twig->render('front/contact.twig'); });
+    $Router->get('/connexion', function () use ($Twig) { echo $Twig->render('front/login.twig'); });
+    $Router->get('/inscription', function () use ($Twig) { echo $Twig->render('front/register.twig'); });*/
 
-// ADMIN
-/*$Router->get('/admin', function () use ($Twig) { echo $Twig->render('admin/dashboard.twig'); });
-$Router->post('/admin', 'Users#login');
+    // ADMIN
+    /*$Router->get('/admin', function () use ($Twig) { echo $Twig->render('admin/dashboard.twig'); });
+    $Router->post('/admin', 'Users#login');
 
-$Router->get('/admin/articles', 'PostsAdmin#listAll');
-$Router->get('/admin/article/ajouter', 'PostsAdmin#create');
-$Router->get('/admin/article/modifier/:id', 'PostsAdmin#update')->with('id', '[0-9]+');
-$Router->get('/admin/article/supprimer/:id', 'PostsAdmin#delete')->with('id', '[0-9]+');
+    $Router->get('/admin/articles', 'PostsAdmin#listAll');
+    $Router->get('/admin/article/ajouter', 'PostsAdmin#create');
+    $Router->get('/admin/article/modifier/:id', 'PostsAdmin#update')->with('id', '[0-9]+');
+    $Router->get('/admin/article/supprimer/:id', 'PostsAdmin#delete')->with('id', '[0-9]+');
 
-$Router->get('/admin/categories', 'CategoriesAdmin#listAll');
-$Router->get('/admin/categories/ajouter', 'CategoriesAdmin#create');
-$Router->get('/admin/categories/modifier/:id', 'CategoriesAdmin#update')->with('id', '[0-9]+');
-$Router->get('/admin/categories/supprimer/:id', 'CategoriesAdmin#delete')->with('id', '[0-9]+');
+    $Router->get('/admin/categories', 'CategoriesAdmin#listAll');
+    $Router->get('/admin/categories/ajouter', 'CategoriesAdmin#create');
+    $Router->get('/admin/categories/modifier/:id', 'CategoriesAdmin#update')->with('id', '[0-9]+');
+    $Router->get('/admin/categories/supprimer/:id', 'CategoriesAdmin#delete')->with('id', '[0-9]+');
 
-$Router->get('/admin/users', 'UsersAdmin#listAll');
-$Router->get('/admin/users/ajouter', 'UsersAdmin#create');
-$Router->get('/admin/users/modifier/:id', 'UsersAdmin#update')->with('id', '[0-9]+');
-$Router->get('/admin/users/desactiver/:id', 'UsersAdmin#deactivate')->with('id', '[0-9]+');*/
+    $Router->get('/admin/users', 'UsersAdmin#listAll');
+    $Router->get('/admin/users/ajouter', 'UsersAdmin#create');
+    $Router->get('/admin/users/modifier/:id', 'UsersAdmin#update')->with('id', '[0-9]+');
+    $Router->get('/admin/users/desactiver/:id', 'UsersAdmin#deactivate')->with('id', '[0-9]+');*/
 
-// EXEC
-$Router->run();
+    // EXEC
+    $Router->run();
+} catch (App\Core\RouterException $RouterException) {
+    $RouterException->display(404);
+}
