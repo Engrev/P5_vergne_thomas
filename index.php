919471fe9e7bd259a8a15ff671332290be6bdc31
Engrev <tvergne83@gmail.com>
2020-05-06 10:37:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,43 +16,43 @@ $Session = App\Core\Session::getInstance();
 $Session->write('Twig', $Twig);
 $Router = new App\Core\Router($_GET['url']);
 
+// FRONT
+$Router->get('/', 'Home#display');
+
+$Router->get('/derniers-articles', 'Posts#last');
+$Router->get('/articles/:id-:slug.:ext', 'Posts#list')->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+')->with('ext', '[a-z]{2,4}');
+
+$Router->get('/categories', 'Categories#listAll');
+$Router->get('/categories/:id-:slug', 'Categories#list')->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+');
+
+/*$Router->get('/contact', function () use ($Twig) { echo $Twig->render('front/contact.twig'); });*/
+$Router->get('/connexion', 'Home#displaySignin');
+$Router->get('/inscription', 'Home#displaySignup');
+$Router->get('/mot-de-passe-oublie', 'Users#forgotPassword');
+
+// ADMIN
+$Router->get('/admin/dashboard', function () use ($Twig) { echo $Twig->render('admin/dashboard.twig'); });
+$Router->post('/admin/signin', 'Users#signIn');
+$Router->post('/admin/signup', 'Users#signUp');
+
+/*$Router->get('/admin/articles', 'PostsAdmin#listAll');
+$Router->get('/admin/article/ajouter', 'PostsAdmin#create');
+$Router->get('/admin/article/modifier/:id', 'PostsAdmin#update')->with('id', '[0-9]+');
+$Router->get('/admin/article/supprimer/:id', 'PostsAdmin#delete')->with('id', '[0-9]+');
+
+$Router->get('/admin/categories', 'CategoriesAdmin#listAll');
+$Router->get('/admin/categories/ajouter', 'CategoriesAdmin#create');
+$Router->get('/admin/categories/modifier/:id', 'CategoriesAdmin#update')->with('id', '[0-9]+');
+$Router->get('/admin/categories/supprimer/:id', 'CategoriesAdmin#delete')->with('id', '[0-9]+');
+
+$Router->get('/admin/users', 'UsersAdmin#listAll');
+$Router->get('/admin/users/ajouter', 'UsersAdmin#create');
+$Router->get('/admin/users/modifier/:id', 'UsersAdmin#update')->with('id', '[0-9]+');
+$Router->get('/admin/users/desactiver/:id', 'UsersAdmin#deactivate')->with('id', '[0-9]+');*/
+
 try {
-    // FRONT
-    $Router->get('/', 'Home#display');
-
-    $Router->get('/derniers-articles', 'Posts#last');
-    $Router->get('/articles/:id-:slug.:ext', 'Posts#list')->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+')->with('ext', '[a-z]{2,4}');
-
-    $Router->get('/categories', 'Categories#listAll');
-    $Router->get('/categories/:id-:slug', 'Categories#list')->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+');
-
-    /*$Router->get('/contact', function () use ($Twig) { echo $Twig->render('front/contact.twig'); });*/
-    $Router->get('/connexion', 'Home#displaySignin');
-    $Router->get('/inscription', 'Home#displaySignup');
-    $Router->get('/mot-de-passe-oublie', 'Users#forgotPassword');
-
-    // ADMIN
-    $Router->get('/admin/dashboard', function () use ($Twig) { echo $Twig->render('admin/dashboard.twig'); });
-    $Router->post('/admin/signin', 'Users#signIn');
-    $Router->post('/admin/signup', 'Users#signUp');
-
-    /*$Router->get('/admin/articles', 'PostsAdmin#listAll');
-    $Router->get('/admin/article/ajouter', 'PostsAdmin#create');
-    $Router->get('/admin/article/modifier/:id', 'PostsAdmin#update')->with('id', '[0-9]+');
-    $Router->get('/admin/article/supprimer/:id', 'PostsAdmin#delete')->with('id', '[0-9]+');
-
-    $Router->get('/admin/categories', 'CategoriesAdmin#listAll');
-    $Router->get('/admin/categories/ajouter', 'CategoriesAdmin#create');
-    $Router->get('/admin/categories/modifier/:id', 'CategoriesAdmin#update')->with('id', '[0-9]+');
-    $Router->get('/admin/categories/supprimer/:id', 'CategoriesAdmin#delete')->with('id', '[0-9]+');
-
-    $Router->get('/admin/users', 'UsersAdmin#listAll');
-    $Router->get('/admin/users/ajouter', 'UsersAdmin#create');
-    $Router->get('/admin/users/modifier/:id', 'UsersAdmin#update')->with('id', '[0-9]+');
-    $Router->get('/admin/users/desactiver/:id', 'UsersAdmin#deactivate')->with('id', '[0-9]+');*/
-
     // EXEC
     $Router->run();
-} catch (App\Core\RouterException $RouterException) {
+} catch (App\Exceptions\RouterException $RouterException) {
     $RouterException->display(404);
 }
