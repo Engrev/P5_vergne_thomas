6946c8655b79900de036a5a4e8b8aa598efccccc
Engrev <tvergne83@gmail.com>
2020-05-15 14:47:38 +0200

    Updates :art:

--- controllers/Controllers.php
+++ controllers/Controllers.php
@@ -62,12 +62,13 @@ class Controllers
             $User = $this->session->read('User');
             if (!is_null($User)) {
                 $data['user']['id'] = $User->getIdUser();
+                $data['user']['id_group'] = $User->getIdGroup();
                 $data['user']['lastname'] = $User->getLastname();
                 $data['user']['firstname'] = $User->getFirstname();
                 $data['user']['email'] = $User->getEmail();
                 $data['user']['avatar'] = $User->getAvatar();
             }
-            $data['categories'] = $this->categories_manager->listAll();
+            $data['categoriesL'] = $this->categories_manager->listAll();
             $data['number_posts'] = $this->posts_manager->countPostsCategory();
             echo $this->twig->render($type.'/'.$template.'.twig', $data);
         }
